2632c6ab106aec8f0343c1bb66d6d87aed97cb9f
DELL <DELL@DELL-PC>
2018-11-12 00:00:17 +0800

    update FP Tree

--- ML/machineLearningInAction/FPTree/FPtree.py
+++ ML/machineLearningInAction/FPTree/FPtree.py
@@ -29,17 +29,31 @@ def loadSimpDat():
                ['r', 'x', 'n', 'o', 's'],
                ['y', 'r', 'x', 'z', 'q', 't', 'p'],
                ['y', 'z', 'x', 'e', 'q', 's', 't', 'm']]
+#     simpDat = [['A', 'B','C', 'E','F','O'],
+#                ['A', 'C', 'G'],
+#                ['E', 'I'],
+#                ['A', 'C', 'D', 'E', 'G'],
+#                ['A', 'C', 'E', 'G', 'L'],
+#                ['E', 'J'],
+#                ['A', 'B','C', 'E','F', 'P'],
+#                ['A', 'C','D'],
+#                ['A', 'C','E', 'G','M'],
+#                ['A', 'C','E', 'G','N']]
     return simpDat
 
 def createInitSet(dataSet):
     retDict = {}
     for data in dataSet:
-        retDict[frozenset(data)] = 1
+        if frozenset(data) not in retDict.keys():
+            retDict[frozenset(data)] = 1
+        else:retDict[frozenset(data)] += 1
     return retDict
 
 def updateHeader(nodeToTest,targetNode):
+    #利用循环找到链表顶端指针为None的节点
     while(nodeToTest.nodeLink!=None):
         nodeToTest = nodeToTest.nodeLink
+    #赋值给链表顶端指针为None的节点
     nodeToTest.nodeLink = targetNode
 
 def updateTree(items,inTree,headerTable,count):
@@ -64,7 +78,7 @@ def createTree(dataSet,minSup=1):
     #第一遍扫描数据，生成项头表，剔除非频繁项集
     for data in dataSet:
         for item in data:
-            headerTable[item] = headerTable.get(item, 0) + 1
+            headerTable[item] = headerTable.get(item, 0) + dataSet[data]
     for k in list(headerTable.keys()):
         if headerTable[k] < minSup:
             del[headerTable[k]]
@@ -88,9 +102,54 @@ def createTree(dataSet,minSup=1):
             updateTree(sortedItems,retTree,headerTable,count)
     return retTree,headerTable
             
-            
+def ascendTree(leafNode,prefixPath):     
+    if leafNode.parent != None:
+        prefixPath.append(leafNode.name)
+        ascendTree(leafNode.parent,prefixPath)
+
+     
+def findPrefixPath(basePat,treeNode):
+    condPats={}
+    while treeNode != None:
+        prefixPath = []
+        #找到一条路径条件模式基
+        ascendTree(treeNode,prefixPath)
+        if len(prefixPath) > 1:
+            condPats[frozenset(prefixPath[1:])] = treeNode.count
+        #切换为另一个相同元素
+        treeNode = treeNode.nodeLink
+    return condPats
+
+#挖掘频繁项集
+def mineTree(inTree, headerTable, minSup,prefix,freqItemList):
+    #从树的底部开始挖掘，支持度由小到大排序
+    bigL = [v[0] for v in sorted(headerTable.items(),key=lambda x:x[1][0])]
+    for basePat in bigL:
+        newFreqSet = prefix.copy()
+        newFreqSet.add(basePat)
+        freqItemList.append(newFreqSet)
+        #各元素的所有路径（条件模式基）并将该元素的支持度赋给所有parent节点
+        condPattBases = findPrefixPath(basePat,headerTable[basePat][1])
+        #构建FPTree剔除条件模式基中小于支持度的元素
+#         myCondTree,myHead = createTree(condPattBases,minSup)
+#         if myHead != None:
+#             #递归调用如[A:2->C:2->D:2]->[A,D][C,D][A,C,D]
+#             mineTree(myCondTree,myHead,minSup,newFreqSet,freqItemList)
             
 dataSet = loadSimpDat()
 initSet = createInitSet(dataSet)
 myTree, myHeaderTable = createTree(initSet,3)
-myTree.display()
+# myTree.display()
+freqItems = []
+mineTree(myTree,myHeaderTable,3,set([]),freqItems)
+print(freqItems)
+# dataList = []
+# with open("kosarak.dat") as f:
+#     for line in f:
+#         dataList.append(line.split())
+# initSet = createInitSet(dataList)
+# myTree, myHeaderTable = createTree(initSet,100000)
+# freqItems = []
+# mineTree(myTree,myHeaderTable,100000,set([]),freqItems)
+# print(freqItems)
+        
