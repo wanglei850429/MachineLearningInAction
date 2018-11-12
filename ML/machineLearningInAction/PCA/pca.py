b330d021ac2a88c1348d10b0c0202adee923f396
DELL <DELL@DELL-PC>
2018-11-12 23:38:21 +0800

    add SVD in PCA

--- ML/machineLearningInAction/PCA/pca.py
+++ ML/machineLearningInAction/PCA/pca.py
@@ -17,37 +17,42 @@ def pca(dataMat,topNFeat):
     centralize = dataMat - meanValues
 #     stdValues = np.std(centralize)
 #     standard = dataMat/stdValues
-    covMat = np.cov(centralize,rowvar=0)
-    eigValues,eigVectors = np.linalg.eig(np.mat(covMat))
-    eigValueIndex = np.argsort(eigValues)
-    eigValueIndex = eigValueIndex[:-(topNFeat+1):-1]
-    regEigVectors = eigVectors[:,eigValueIndex]
-    lowDDataMat = centralize * regEigVectors
+    #基于协方差矩阵
+#     covMat = np.cov(centralize,rowvar=0)
+#     eigValues,eigVectors = np.linalg.eig(np.mat(covMat))
+#     eigValueIndex = np.argsort(eigValues)
+#     eigValueIndex = eigValueIndex[:-(topNFeat+1):-1]
+#     regEigVectors = eigVectors[:,eigValueIndex]
+#     lowDDataMat = centralize * regEigVectors
+    #基于SVD
+    u,sigma,vt = np.linalg.svd(centralize)
+    lowDDataMat = centralize*np.mat(vt[0,:]).T
+    regEigVectors = np.mat(vt[0,:]).T
     return lowDDataMat,lowDDataMat*regEigVectors.T+meanValues
 
-# dataMat = loadDataSet("testSet.txt")
-# lowDMat,chgMat = pca(dataMat,1)
-#  
-# fig = plt.figure()
-# ax = fig.add_subplot(111)
-# ax.scatter(dataMat[:,0].flatten().A[0],dataMat[:,1].flatten().A[0],marker="^",s=90)
-# ax.scatter(chgMat[:,0].flatten().A[0],chgMat[:,1].flatten().A[0],marker="o",s=50,c="red")
-# plt.show()
+dataMat = loadDataSet("testSet.txt")
+lowDMat,chgMat = pca(dataMat,1)
+  
+fig = plt.figure()
+ax = fig.add_subplot(111)
+ax.scatter(dataMat[:,0].flatten().A[0],dataMat[:,1].flatten().A[0],marker="^",s=90)
+ax.scatter(chgMat[:,0].flatten().A[0],chgMat[:,1].flatten().A[0],marker="o",s=50,c="red")
+plt.show()
 
-def replaceNanWithMean():
-    dataMat = loadDataSet("secom.data", " ")
-    numFeat = dataMat.shape[1]
-    for i in range(numFeat):
-        meanValue = np.mean(dataMat[np.nonzero(~np.isnan(dataMat[:,i].A))[0],i])
-        dataMat[np.nonzero(np.isnan(dataMat[:,i].A))[0],i] = meanValue
-    return dataMat
- 
-dataMat = replaceNanWithMean()
-meanValues = np.mean(dataMat,0)
-centralize = dataMat - meanValues
-cov = np.cov(centralize, rowvar=0)
-eigValues,eigVector = np.linalg.eig(np.mat(cov))
-print(sorted(eigValues,reverse=True))
+# def replaceNanWithMean():
+#     dataMat = loadDataSet("secom.data", " ")
+#     numFeat = dataMat.shape[1]
+#     for i in range(numFeat):
+#         meanValue = np.mean(dataMat[np.nonzero(~np.isnan(dataMat[:,i].A))[0],i])
+#         dataMat[np.nonzero(np.isnan(dataMat[:,i].A))[0],i] = meanValue
+#     return dataMat
+#  
+# dataMat = replaceNanWithMean()
+# meanValues = np.mean(dataMat,0)
+# centralize = dataMat - meanValues
+# cov = np.cov(centralize, rowvar=0)
+# eigValues,eigVector = np.linalg.eig(np.mat(cov))
+# print(sorted(eigValues,reverse=True))
 
 
 
